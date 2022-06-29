a47338a140691f44560a2f91ea48fd0af2347c73
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-29 16:35:44 +0530

    [Madhavi] employee payroll testing

--- EmployeePayrollService/EmployeePayrollTesting/UnitTest1.cs
+++ EmployeePayrollService/EmployeePayrollTesting/UnitTest1.cs
@@ -8,14 +8,14 @@ namespace EmployeePayrollTesting
        
         EmployeeRepo getMethod = new EmployeeRepo();
         [Test]
-        public void GivenMultipleEmployeeDetails_ShouldReturnTatalExecutionTime()
+        public void GivenMultipleEmployeeDetails_ShouldReturnTotalExecutionTime()
         {
             List<EmployeePayroll_Model> list = new List<EmployeePayroll_Model>();
-            list.Add(new EmployeePayroll_Model(id: 0, name: "Saniya", salary: 15000, startDate: new DateTime(2021, 05, 01), gender: "F", mobile: 9835646725, address: "Sangavi", department: "Developer", basicPay: 15000, deductions: 200, taxablePay: 500, netPay: 14500));
-            list.Add(new EmployeePayroll_Model(id: 0, name: "Pallavi", salary: 15000, startDate: new DateTime(2021, 04, 01), gender: "F", mobile: 7864667742, address: "Phaltan", department: "Developer", basicPay: 15000, deductions: 200, taxablePay: 500, netPay: 14500));
-            list.Add(new EmployeePayroll_Model(id: 0, name: "Pooja", salary: 15000, startDate: new DateTime(2021, 05, 01), gender: "F", mobile: 8873752538, address: "Baramati", department: "Developer", basicPay: 15000, deductions: 200, taxablePay: 500, netPay: 14500));
-            list.Add(new EmployeePayroll_Model(id: 0, name: "Sonal", salary: 15000, startDate: new DateTime(2021, 06, 01), gender: "F", mobile: 9978453867, address: "Bhigwan", department: "Developer", basicPay: 15000, deductions: 200, taxablePay: 500, netPay: 14500));
-            list.Add(new EmployeePayroll_Model(id: 0, name: "Sakshi", salary: 15000, startDate: new DateTime(2021, 03, 01), gender: "F", mobile: 8798656473, address: "Pandare", department: "Developer", basicPay: 15000, deductions: 200, taxablePay: 500, netPay: 14500));
+            list.Add(new EmployeePayroll_Model(id: 0, name: "Thamarai", salary: 20000, startDate: new DateTime(2022, 06, 01), gender: "M", mobile: 9877065432, address: "Tirupur", department: "Developer", basicPay: 20000, deductions: 200, taxablePay: 500, netPay: 19500));
+            list.Add(new EmployeePayroll_Model(id: 0, name: "Subash", salary: 20000, startDate: new DateTime(2022, 05, 01), gender: "M", mobile: 7123456098, address: "Coimbatore", department: "Developer", basicPay: 20000, deductions: 200, taxablePay: 500, netPay: 19500));
+            list.Add(new EmployeePayroll_Model(id: 0, name: "Anand", salary: 20000, startDate: new DateTime(2022, 04, 01), gender: "M", mobile: 6543210987, address: "Neyveli", department: "Developer", basicPay: 20000, deductions: 200, taxablePay: 500, netPay: 19500));
+            list.Add(new EmployeePayroll_Model(id: 0, name: "Guna", salary: 20000, startDate: new DateTime(2022, 03, 01), gender: "M", mobile: 7564321098, address: "Ariyalur", department: "Developer", basicPay: 20000, deductions: 200, taxablePay: 500, netPay: 19500));
+            list.Add(new EmployeePayroll_Model(id: 0, name: "Prithiv", salary: 20000, startDate: new DateTime(2022, 02, 01), gender: "M", mobile: 8970645321, address: "MettuPalayam", department: "Developer", basicPay: 20000, deductions: 200, taxablePay: 500, netPay: 19500));
             DateTime startTime = DateTime.Now;
             getMethod.AddMultipleEmployees(list);
             DateTime endTime = DateTime.Now;
